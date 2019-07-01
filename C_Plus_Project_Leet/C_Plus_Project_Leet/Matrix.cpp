9f05777d8d08623fe37f1c50ad8a09e069e40d07
slq <15097505768@163.com>
2019-07-01 18:57:50 +0800

    // 36. Valid Sudoku 没做完

--- C_Plus_Project_Leet/C_Plus_Project_Leet/Matrix.cpp
+++ C_Plus_Project_Leet/C_Plus_Project_Leet/Matrix.cpp
@@ -10,9 +10,53 @@ using namespace std;
 
 #include "Matrix.hpp"
 #include <vector>
+#include <queue>
 #include "Array.hpp"
 
 // 378. Kth Smallest Element in a Sorted Matrix
+// 大根堆
 int kthSmallest(vector<vector<int>>& matrix, int k) {
-    
+    priority_queue<int> queue;
+    for (int i = 0 ; i < matrix.size(); i++) {
+        for (int j = 0; j < matrix[0].size(); j++) {
+            queue.push(matrix[i][j]);
+            if (queue.size() == k+1) queue.pop();
+        }
+    }
+    return queue.top();
+}
+
+// 36. Valid Sudoku
+bool isValidSudoku(vector<vector<char>>& board) {
+    vector<int> rowMarks(9,0);
+    vector<int> colMarks(9,0);
+    vector<int> subSudoMarks(9,0);
+    for (int row = 0; row < 9; row++) {
+        for (int col = 0; col < 9; col++) {
+            if (board[row][col] == '.') {
+                continue;
+            }
+            int digit = board[row][col] - '0';
+            if (digit < 1 || digit > 9) {
+                return false;
+            }
+            if (( (1 << digit) & rowMarks[digit] ) == 0) {
+                rowMarks[digit] |= (1 << digit);
+            }else {
+                return false;
+            }
+            if (( (1 << digit) & colMarks[digit] ) == 0) {
+                colMarks[digit] |= (1 << digit);
+            }else {
+                return false;
+            }
+            
+            if (( (1 << digit) & subSudoMarks[(row/3)*3+col/3] ) == 0) {
+                subSudoMarks[(row/3)*3+col/3] |= (1 << digit);
+            }else {
+                return false;
+            }
+        }
+    }
+    return true;
 }
